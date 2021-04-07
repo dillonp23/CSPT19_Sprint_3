1b335cb057f6f4176d8c731d19bc92149aac881a
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-06 20:59:33 -0500

    Added notes file for Binary Trees lecture

---
+++ 3.1/lecture_notes.py
@@ -0,0 +1,10 @@
+
+
+"""
+Sprint 3.1 - Binary Trees & Binary Search Trees
+
+* Binary Trees are comprised of nodes, subtrees, and leaves
+    - parent node can have up to 2 child nodes - left or right
+    - leaves are nodes with no children
+
+"""
