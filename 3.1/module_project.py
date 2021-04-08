8a83c5a736755b6fa99531ea93ff9608f2146262
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-07 23:10:58 -0500

    Added prompt for exercise 2

--- 3.1/module_project.py
+++ 3.1/module_project.py
@@ -75,3 +75,25 @@ def isBalanced(left, right):
 
     if right:
         return isBalanced(right.left, right.right)
+
+
+
+
+
+"""
+Exercise 2: Minimum Depth Binary Tree
+
+You are given a binary tree and you are asked to write a function that finds its minimum depth. The minimum 
+depth can be defined as the number of nodes along the shortest path from the root down to the nearest leaf 
+node. As a reminder, a leaf node is a node with no children.
+
+* Example:
+    Given the binary tree [5,7,22,None,None,17,9],
+
+        5
+       / \
+      7  22
+        /  \
+       17   9
+    your function should return its minimum depth = 2.
+"""
