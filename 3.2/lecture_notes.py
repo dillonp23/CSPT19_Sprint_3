74239d2e61194eff08860776a3f821ec1b310c3b
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-08 21:03:39 -0500

    Added solution for exercise 1

--- 3.2/lecture_notes.py
+++ 3.2/lecture_notes.py
@@ -2,13 +2,16 @@
 
 """
 Sprint 3.2 Lecture Notes - Exploring Tree Traversals
+
+
+
 """
 
 
 
 
 """
-Exercise: "104. Maximum Depth of Binary Tree" (https://leetcode.com/problems/maximum-depth-of-binary-tree/)
+Exercise 1: "104. Maximum Depth of Binary Tree" (https://leetcode.com/problems/maximum-depth-of-binary-tree/)
 
 Given the root of a binary tree, return its maximum depth.
 
@@ -27,9 +30,42 @@ A binary tree's maximum depth is the number of nodes along the longest path from
 
     Input: root = [0]
     Output: 1
+
+
+* UPER - Plan:
+
+    * keywords: max, depth
+
+    - keywords hint at a depth first solution
+    - depth = count of nodes from root to furthest leaf
+    - use iterative approach with a stack
+    - if parent has a child, add to stack and add the depth of the node
+    - use a max comparison check to update and keep of the maximum depth for any node as we traverse
 """
 
+from collections import deque
 
+def maxDepth(root):
+    if root is None:
+        return 0
+
+    stack = deque()
+    stack.append((root, 1))
+    maxDepth = 1
+
+    while len(stack) > 0:
+        curr = stack.pop()
+        currNode, currDepth = curr[0], curr[1]
+
+        maxDepth = max(maxDepth, currDepth)
+
+        if currNode.left:
+            stack.append((currNode.left, currDepth + 1))
+
+        if currNode.right:
+            stack.append((currNode.right, currDepth + 1))
+
+    return maxDepth
 
 
 """
