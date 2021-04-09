4c052bffd9e9b99b7f3d39e2d7f3e79575889841
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-08 19:22:42 -0500

    Updated prompt for exercise 5

--- 3.1/lecture_notes.py
+++ 3.1/lecture_notes.py
@@ -487,14 +487,17 @@ Exercise 5: "98. Validate Binary Search Tree (https://leetcode.com/problems/vali
 
 Given the root of a binary tree, determine if it is a valid binary search tree (BST).
 
-A valid BST is defined as follows:
-
-The left subtree of a node contains only nodes with keys less than the node's key.
-The right subtree of a node contains only nodes with keys greater than the node's key.
-Both the left and right subtrees must also be binary search trees.
+* A valid BST is defined as follows:
+    - left subtree of a node contains only nodes with keys less than the node's key
+    - right subtree of a node contains only nodes with keys greater than the node's key
+    - both the left and right subtrees must also be binary search trees
  
 
-* Example 1:
+* Examples:
     Input: root = [2,1,3]
     Output: true
+
+    Input: root = [5,1,4,null,null,3,6]
+    Output: false
+    Explanation: The root node's value is 5 but its right child's value is 4.
 """
