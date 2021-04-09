370c034701fc72842bb94f720ca41246b5c5aa58
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-08 20:31:46 -0500

    Added prompt for second exercise from lecture notes

--- 3.2/lecture_notes.py
+++ 3.2/lecture_notes.py
@@ -6,6 +6,7 @@ Sprint 3.2 Lecture Notes - Exploring Tree Traversals
 
 
 
+
 """
 Exercise: "104. Maximum Depth of Binary Tree" (https://leetcode.com/problems/maximum-depth-of-binary-tree/)
 
@@ -18,15 +19,30 @@ A binary tree's maximum depth is the number of nodes along the longest path from
     Input: root = [3,9,20,null,null,15,7]
     Output: 3
 
-
     Input: root = [1,null,2]
     Output: 2
-
 
     Input: root = []
     Output: 0
 
-
     Input: root = [0]
     Output: 1
+"""
+
+
+
+
+"""
+Exercise: "230. Kth Smallest Element in a BST" (https://leetcode.com/problems/kth-smallest-element-in-a-bst/)
+
+Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+
+* Examples:
+
+    Input: root = [3,1,4,null,2], k = 1
+    Output: 1
+
+
+    Input: root = [5,3,6,2,4,null,null,1], k = 3
+    Output: 3
 """
