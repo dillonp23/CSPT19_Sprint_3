36c6eed03618ae86bab7919590c981603d998272
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-10 19:11:11 -0500

    Adds new file to practice binary tree exercises. Ads exercise 1 and 2 from lecture 3.2

---
+++ 3.2/additional_practice.py
@@ -0,0 +1,40 @@
+
+
+"""
+Exercise 1: "104. Maximum Depth of Binary Tree" (https://leetcode.com/problems/maximum-depth-of-binary-tree/)
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+* Examples:
+
+    Input: root = [3,9,20,null,null,15,7]
+    Output: 3
+
+    Input: root = [1,null,2]
+    Output: 2
+
+    Input: root = []
+    Output: 0
+
+    Input: root = [0]
+    Output: 1
+"""
+
+
+
+
+"""
+Exercise 2: "230. Kth Smallest Element in a BST" (https://leetcode.com/problems/kth-smallest-element-in-a-bst/)
+
+Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+
+* Examples:
+
+    Input: root = [3,1,4,null,2], k = 1
+    Output: 1
+
+    Input: root = [5,3,6,2,4,null,null,1], k = 3
+    Output: 3
+"""
