38f96aad8f25d187f5a656c9a21d9cfe3d676170
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-10 19:49:57 -0500

    Added UPER steps and function definitions for max depth exercise 1, as well as TreeNode class definition

--- 3.2/additional_practice.py
+++ 3.2/additional_practice.py
@@ -20,7 +20,43 @@ A binary tree's maximum depth is the number of nodes along the longest path from
 
     Input: root = [0]
     Output: 1
+
+
+* UPER - Understand:
+
+    * keywords: maximum, depth
+
+    - for a given node, its depth is equal to the sum of all parent nodes before it + 1
+    - node with max depth will be a leaf, i.e (node.left and node.right) == None
+    - interested in a node with no children, therefore one good approach would be a post-order traversal
+        - i.e. if a given nodes children aren't the max depth, then neither is itself
+        - allows us to eliminate any parent nodes ==> if a node has children, don't compare it with max
+    - each time we move down a level, iterate depth + 1
+    - children will inherit depth from parent
+    
+
+* UPER - Plan:
+    - traverse list in pre-order fashion
+    - use recursion to pass root and a depth variable
+    - whenever we get to a node with no children return its depth
+    - at each parent node get a local max by comparing depth of its left and right children
+    - local max of a parent node can then be compared to a global max
+        - if local max is greater than global max => update global
+        - if local max < global, we know any parent node to current node is also less than global max
+    - if at a leaf node, return its depth
+    - otherwise if at a parent node return its max
 """
+class TreeNode:
+    def __init__(self, x):
+        self.value = x
+        self.left = None
+        self.right = None
+
+
+def maxDepth(self, root):
+    pass
+
+
 
 
 
