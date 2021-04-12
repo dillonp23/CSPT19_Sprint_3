e2a4c881eceac81ec009304b50a623556b2d8ac7
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-11 21:29:51 -0500

    Added prompt for exercise 2

--- 3.2/module_project.py
+++ 3.2/module_project.py
@@ -120,3 +120,54 @@ def inOrderTraversalHelper(root, node_vals):
         inOrderTraversalHelper(root.right, node_vals)
         
     return node_vals
+
+
+
+
+
+"""
+Exercise 2: Traverse Tree (task 8 of 9)
+
+Given a binary tree of integers t, return its node values in the following format:
+
+    - The first element should be the value of the tree root
+    - The next elements should be the values of the nodes at height 1 (i.e. the root children), ordered from the leftmost to the rightmost
+    - The elements after that should be the values of the nodes at height 2 (i.e. the children of the nodes at height 1) ordered in the same way
+
+* Note:
+    Solve this task without using recursion, since this is what you'll be asked to do during an interview.
+
+* Example:
+
+    For tree:
+
+            1
+          /   \
+         2     4
+          \   /
+           3 5
+
+    input: t = {
+        "value": 1,
+        "left": {
+            "value": 2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": {
+            "value": 4,
+            "left": {
+                "value": 5,
+                "left": null,
+                "right": null
+            },
+            "right": null
+        }
+    }
+
+    output: [1, 2, 4, 3, 5]
+"""
