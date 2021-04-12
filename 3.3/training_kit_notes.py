6617adbc60075eeb4f117b77a98f1d0f850098da
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-12 12:02:36 -0500

    Added initial notes about the basics of graphs and how they can be used

--- 3.3/training_kit_notes.py
+++ 3.3/training_kit_notes.py
@@ -2,4 +2,35 @@
 
 """
 Sprint 3.3 - Graphs I
+
+
+Objective 1: Introduction to Graphs
+
+* Graphs
+    - collections of related data
+    - similar to trees
+    - connections can be made from a node to any other node in graph
+    - can be in the form of a loop
+    - all trees are graphs, but not all graphs are trees
+
+
+    * Building Blocks
+        1. vertices
+            - nodes in a graph are called "vertices" or "verts"
+        
+        2. edges
+            - connections in a graph are called "edges"
+            - an edge denotes a relationship or linkage between two verts
+
+
+    * Useful Applications
+        - can be used for a wide range of applications
+        - easily represent any multi-directional relational data
+        
+        * examples:
+            - cities and their linking roads
+            - computers on a network
+            - populations of people who know each other and their network ("six degrees")
+            - trade relationships between countries
+            - money owed in an ongoing poker tournament
 """
