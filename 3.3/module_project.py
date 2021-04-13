897c75b206b77b01a6f1cd4074ed9a76156db7b1
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-13 15:59:46 -0500

    Updated prompt for quiz 1 and added prompt for quiz 2

--- 3.3/module_project.py
+++ 3.3/module_project.py
@@ -7,9 +7,10 @@ Sprint 3.3 - Graphs I CodeSignal Assignment
 
 
 """
-Quiz (task 3 of 5): Representing a graph with an adjacency list
+Quiz I (task 3 of 5): Representing a graph with an adjacency list
 
-Given this graph:
+Given the graph in this image:
+
     https://codesignal.s3.amazonaws.com/uploads/1601408702086/68747470733a2f2f692e696d6775722e636f6d2f634a366c656b4d2e6a7067.jpeg
 
     Use an adjacency list to represent this graph.
@@ -28,4 +29,17 @@ Given this graph:
                 "F": {},
                 "G": {"D": 1}
             }
+"""
+
+
+
+
+
+"""
+Quiz II (task 4 of 5): Graph representation in Python interpreter
+
+You are given a Vertex and Graph class. Choose the Python code (written in an interactive interpreter) that would
+correctly create the graph in this image:
+
+    https://codesignal.s3.amazonaws.com/uploads/1601409897247/68747470733a2f2f692e696d6775722e636f6d2f796931503441462e6a7067.jpeg
 """
