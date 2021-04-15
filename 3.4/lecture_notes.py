8a7136f321929afacefb29a38a91de4de667aa02
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-14 21:29:17 -0500

    Added prompt for exercise 2

--- 3.4/lecture_notes.py
+++ 3.4/lecture_notes.py
@@ -142,3 +142,35 @@ print(allPathsSourceTargetRecursive([[1],[2],[3],[4],[5],[]])) # expected: [[0, 
 print(allPathsSourceTargetRecursive([[1,3,7],[7],[5],[5],[],[7],[],[]])) # expected: [[0, 1, 7], [0, 3, 5, 7], [0, 7]]
 print(allPathsSourceTargetRecursive([[1, 2],[3],[3],[]])) # expected: [[0, 1, 3], [0, 2, 3]]
 print(allPathsSourceTargetRecursive([[1, 2],[3],[3],[4],[5],[6,8],[9],[9],[9],[]]))
+
+
+
+
+"""
+Exercise 2: "733. Flood Fill" (https://leetcode.com/problems/flood-fill/)
+
+An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, 
+"flood fill" the image.
+
+To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of 
+the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color 
+as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+At the end, return the modified image.
+
+* Example:
+
+    Input: 
+    image = [[1,1,1],[1,1,0],[1,0,1]]
+    sr = 1, sc = 1, newColor = 2
+
+    Output: [[2,2,2],[2,2,0],[2,0,1]]
+
+    Explanation: 
+    From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+    by a path of the same color as the starting pixel are colored with the new color.
+    Note the bottom corner is not colored 2, because it is not 4-directionally connected
+    to the starting pixel.
+"""
