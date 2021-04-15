c522c561de69b7bf75e1184e0e6ba654bc2fd34f
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-14 20:41:54 -0500

    Added intro to Graphs II notes

--- 3.4/lecture_notes.py
+++ 3.4/lecture_notes.py
@@ -1,4 +1,29 @@
 
 """
 Sprint 3.4 - Graphs II Lecture Notes
+
+
+* Traversal vs Search:
+    - traversal = typically covering entire graph
+    - search = typically find item, terminate early and return
+
+
+* DFS vs BFS:
+    - BFS = most useful for shortest/min path
+
+
+* How to solve any graph problem (an UPER-like framework):
+
+    1. translate into graph terminology
+        - what are my vertices, edges and weight?
+
+    2. how would you build a graph?
+        - do you need to build it or can you use whatever input data is given to you?
+
+    3. How to traverse/search - DFS or BFS?
+        - any auxillary data needed?
+        - what data do I need to store?
+
+
+    * Answering these three steps will help guide you to a solution!
 """
