4523e0c1286326cc080174fedd6d7faf1fba57ec
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-18 18:50:59 -0500

    Added prompt for exercise 1

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -1,3 +1,31 @@
 
 
 """ Sprint Challenge 3 - Data Structures & Algorithms II """
+
+
+"""
+Exercise 1: You are given the root node of a binary search tree (BST).
+
+You need to write a function that returns the sum of values of all the nodes with a value between lower and upper (inclusive).
+
+The BST is guaranteed to have unique values.
+
+* Examples:
+
+    Input:
+        root = [10, 5, 15, 3, 7, null, 18]
+        lower = 7
+        upper = 15
+        
+    Output:
+        32
+
+    
+    Input:
+        root = [10,5,15,3,7,13,18,1,null,6]
+        lower = 6
+        upper = 10
+        
+    Output:
+        23
+"""
