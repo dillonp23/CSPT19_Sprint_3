20d7b77c2bd7e15b64d688ccda469cc0dc54b348
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-18 20:19:39 -0500

    Added solution for exercise 2

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -78,3 +78,23 @@ Example:
      / \   / \
     9   7 5   2
 """
+from collections import deque
+
+def csBinaryTreeInvert(root):
+    queue = deque()
+    queue.append(root)
+
+    while len(queue) > 0:
+        popped = queue.popleft()
+        temp = popped.left
+
+        popped.left = popped.right
+        popped.right = temp
+
+        if popped.left:
+            queue.append(popped.left)
+        
+        if popped.right:
+            queue.append(popped.right)
+
+    return root
